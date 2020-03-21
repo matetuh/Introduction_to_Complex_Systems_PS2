d88795825c88f4b8779e1d103bf3d4279728022d
matetuh <matetuh@gmail.com>
2020-03-21 16:11:25 +0100

    Init Commit

--- site_percolation_problem.py
+++ site_percolation_problem.py
@@ -6,31 +6,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # square lattice side length -> L
-L = 20
+L = 5
 # setting the probability
 p = 0.5
-# defining 
-A = []
+# createing L separate lists o L elements:
+A = [[0]*L for _ in range(L)]
 current = []
 
 # creating 2d loopp, to creating 2d array
 # i - number of rows
-for _ in range(L):
+for i in range(L):
     # j - number of cols
-    current = []
-    for _ in range(L):
+    for j in range(L):
         # if the random number [0,1] is smaller than the set probablity p, than
         if random.random() < p :
             # if yes, set i-row and j-col to be occupied
-            current.append(1)
+            A[i][j] = 1
         else:
             # if no, set it as empty cell
-            current.append(0)
-    A.append(current)
+            A[i][j] = 0
 
 # plotting the array
 plt.imshow(A)
 plt.colorbar()
 plt.show()
 
+#------------- the burning method ----------------
+B = [[None]*L for _ in range(L)]
+t = 2
+# Label all occupied cells in the top line with the marker t=2.
+for j in range(L):
+    B[0][j] = t
 
+print(B)
+        
