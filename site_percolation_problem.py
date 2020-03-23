4069e719b9f924e15d8fd6f3dea8a07670efd84c
matetuh <matetuh@gmail.com>
2020-03-23 20:52:30 +0100

    Init Commit

--- site_percolation_problem.py
+++ site_percolation_problem.py
@@ -9,7 +9,7 @@ import matplotlib
 # square lattice side length -> L
 L = 10
 # setting the probability
-p = 0.7
+p = 0.5
 # createing L separate lists o L elements:
 A = [[0]*L for _ in range(L)]
 current = []
@@ -36,7 +36,7 @@ for i in range(L):
     print(A[i])
 print("-----------------------------")
 #------------- the burning method ----------------
-B = [[0]*(L+1) for _ in range(L+1)]
+""" B = [[0]*(L+1) for _ in range(L+1)]
 t = 2
 # 1.Label all occupied cells in the top line with the marker t=2.
 # zwiększanie macierzy A o jeden rząd i 1 kolumnę, tak żeby możnabyło w kolejnym kroku 
@@ -56,19 +56,26 @@ for i in range(L+1):
 print("-----------------------")
 
 # 2.Go through all the cells and find the cells which have label t.
-for i in range(L):
-    for j in range(L):
-        # dla kolejnych t sprawdzamy 4 kierunki przestrzenne czy pola są zajęte czy wolne
-        if B[i][j] == t:
-            if B[i+1][j] == 1:
-                B[i+1][j] = t + 1
-            if B[i][j+1] == 1:
-                B[i][j+1] = t + 1
-            if B[i-1][j] == 1:
-                B[i-1][j] = t + 1
-            if B[i][j-1] == 1:
-               B[i][j-1] = t + 1
-    t = t + 1
+def func(B, L, t):
+    for i in range(L):
+        for j in range(L):
+            # dla kolejnych t sprawdzamy 4 kierunki przestrzenne czy pola są zajęte czy wolne
+            if B[i][j] == t:
+                if B[i+1][j] == 1:
+                    B[i+1][j] = t + 1
+                    func(B, L, t)
+                if B[i][j+1] == 1:
+                    B[i][j+1] = t + 1
+                    func(B, L, t)
+                if B[i-1][j] == 1:
+                    B[i-1][j] = t + 1
+                    func(B, L, t)
+                if B[i][j-1] == 1:
+                    B[i][j-1] = t + 1
+                    func(B, L, t)
+        t = t + 1
+func(B, L, t)
+
 #print(B)
 
 # zmniejszenie wymiaru macierzy o 1 kolumnę i rząd, przepisanie B na A
@@ -93,4 +100,54 @@ for i in range(L):
                        ha="center", va="center", color="w")
 ax.set_title("Percolation")
 fig.tight_layout()
+plt.show() """
+
+# ------------------ Hoshen-Kopelman algorithm -------------------
+
+fig, ax = plt.subplots()
+im = ax.imshow(A)
+for i in range(L):
+    for j in range(L):
+        text = ax.text(j, i, A[i][j],
+                       ha="center", va="center", color="w")
+ax.set_title("Percolation")
+fig.tight_layout()
+plt.show() 
+
+C = [[0]*(L+1) for _ in range(L+1)]
+for i in range (L):
+    for j in range (L):
+        C[i+1][j+1] = A[i][j]
+
+t = 2
+label = np.zeros((L+1,L+1))
+for i in range(L+1):
+    for j in range(L+1):
+        if (C[i][j]):
+            up = C[i-1][j]
+            left = C[i][j-1]
+            if (up + left == 0):
+                label[i][j] = t
+                t = t + 1
+            elif (up == 0 and left != 0):
+                label[i][j] = label[i][j-1]
+            elif (up != 0 and left == 0):
+                label[i][j] = label[i-1][j]
+            else:
+                label[i][j] = label[i-1][j]
+
+label = np.delete(label,0,0)
+label = np.delete(label,0,1)
+fig, ax = plt.subplots()
+im = ax.imshow(label)
+for i in range(L):
+    for j in range(L):
+        text = ax.text(j, i, int(label[i][j]),
+                       ha="center", va="center", color="w")
+ax.set_title("Percolation")
+fig.tight_layout()
 plt.show()
+
+    
+        
+
